5061a1df649c1fb31b9abed7b80d72c90409c0c5
Henning Spjelkavik <henning@spjelkavik.net>
2018-06-17 21:32:41 +0200

    Added install for sqlserver jar.

--- install.sh
+++ install.sh
@@ -1,1 +1,3 @@
 mvn install:install-file -Dfile=lib/commapi/comm.jar -DgroupId=javax.comm -DartifactId=comm -Dversion=2.0.3 -Dpackaging=jar
+./mvnw install:install-file -Dfile=lib/sqljdbc4.jar -DgroupId=com.microsoft -DartifactId=sqlserver -Dversion=3.0.1301 -Dpackaging=jar
+
