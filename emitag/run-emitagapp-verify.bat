9c631eeed3f417f2d4f1d58286780aeab8fc3bb6
Henning Spjelkavik <henning.spjelkavik@finn.no>
2021-08-10 23:51:34 +0200

    Changed version numbers.

--- emitag/run-emitagapp-verify.bat
+++ emitag/run-emitagapp-verify.bat
@@ -1,1 +1,1 @@
-"c:\Program Files (x86)\java\jre7\bin\java.exe" -DVERIFY=true -DRELAY=true -jar target\serial-250-emitag-3.0.jar
+"c:\Program Files (x86)\java\jre7\bin\java.exe" -DVERIFY=true -DRELAY=true -jar target\serial-250-emitag-3.0-SNAPSHOT.jar
