a240f60e5688f34e0456a64c211619fbd2abd1ec
Henning Spjelkavik <henning.spjelkavik@finn.no>
2023-08-18 22:39:34 +0200

    Build file.

--- build-zip.sh
+++ build-zip.sh
@@ -1,3 +1,3 @@
 (cd .. ; 
-zip -r NightHawk-emitag-2021.zip Emit-Serial-250-Skolesprinten/emitag Emit-Serial-250-Skolesprinten/registy_fix_for_clean_wi* Emit-Serial-250-Skolesprinten/win32com.dll Emit-Serial-250-Skolesprinten/lib/ Emit-Serial-250-Skolesprinten/javax.comm.properties  Emit-Serial-250-Skolesprinten/INSTALL  Emit-Serial-250-Skolesprinten/README.md 
+zip -r NightHawk-emitag-2023.zip Emit-Serial-250-Skolesprinten/emitag Emit-Serial-250-Skolesprinten/registy_fix_for_clean_wi* Emit-Serial-250-Skolesprinten/win32com.dll Emit-Serial-250-Skolesprinten/lib/ Emit-Serial-250-Skolesprinten/javax.comm.properties  Emit-Serial-250-Skolesprinten/INSTALL  Emit-Serial-250-Skolesprinten/README.md 
 )
